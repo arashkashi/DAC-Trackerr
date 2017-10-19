dc5eafa855a2f8100ed73f78852dddef537c18fe
arashkashi@gmail.com <arashkashi@gmail.com>
2017-10-19 17:55:17 +0200

    Initial Commit

---
+++ DAC-TrackerTests/DAC_TrackerTests.swift
@@ -0,0 +1,36 @@
+//
+//  DAC_TrackerTests.swift
+//  DAC-TrackerTests
+//
+//  Created by Arash K. on 2017-10-19.
+//  Copyright © 2017 Arash K. All rights reserved.
+//
+
+import XCTest
+@testable import DAC_Tracker
+
+class DAC_TrackerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
